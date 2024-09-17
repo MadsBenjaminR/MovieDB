18fcac1da4e81e1ebade49bee3796ea507798ff3
moldamolda <danielherlev@hotmail.com>
2024-09-17 12:31:14 +0200

    Hey

--- src/main/java/dat/entities/Movie.java
+++ src/main/java/dat/entities/Movie.java
@@ -1,11 +1,11 @@
 package dat.entities;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 
 /**@author laith kaseb**/
